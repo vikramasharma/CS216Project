6d1e45e4d7bf403525bd87fefc93f57cae1cb27c
vikramasharma <Hanger2#>
2023-12-04 20:43:24 -0500

    updates

--- project.py
+++ project.py
@@ -15,20 +15,22 @@ player_career_info_df = player_career_info_df.merge(
 
 # we may need to address NA's differently in the main analysis depending on what statistic we are looking at using fillna()
 #For simplicity's sake, we will drop all Na's in this example
-player_totals_df.dropna(inplace=True)
-player_shooting_df.dropna(inplace=True)
-player_play_by_play_df.dropna(inplace=True)
-player_career_info_df.dropna(inplace=True)
+# player_totals_df.dropna(inplace=True)
+# player_shooting_df.dropna(inplace=True)
+# player_play_by_play_df.dropna(inplace=True)
+# player_career_info_df.dropna(inplace=True)
 
 avg_years_by_position = player_career_info_df.groupby('pos')['num_seasons'].mean()
 players_by_position = player_totals_df.groupby('pos')['player'].nunique()
+player_totals_df['poscount'] = player_totals_df.groupby('pos')['player'].nunique()
+f_player_totals_df = player_totals_df[player_totals_df['poscount']>30]
+
+f_player_totals_df = player_totals_df.sort_values(by=['player_id', 'season'])
 
 
-player_totals_df = player_totals_df.sort_values(by=['player_id', 'season'])
+f_player_totals_df['pts_diff'] = f_player_totals_df.groupby('player_id')['pts'].diff()
 
-player_totals_df['pts_diff'] = player_totals_df.groupby('player_id')['pts'].diff()
-
-avg_improvement_by_position = player_totals_df.dropna(subset=['pts_diff']).groupby('pos')['pts_diff'].mean()
+avg_improvement_by_position = f_player_totals_df.dropna(subset=['pts_diff']).groupby('pos')['pts_diff'].mean()
 
 # Display the results
 print("Average number of years played by position:")
@@ -40,14 +42,14 @@ print(players_by_position)
 print("\nAverage improvement in points scored by position:")
 print(avg_improvement_by_position)
 
-combined_positions = player_totals_df[player_totals_df['pos'].isin(['SF-SG', 'SG-PF'])]
+combined_positions = f_player_totals_df[f_player_totals_df['pos'].isin(['SF-SG', 'SG-PF'])]
 print(combined_positions[['player', 'season', 'pts', 'pts_diff']])
 
 # Calculate median improvement in points scored by position
-median_improvement_by_position = player_totals_df.dropna(subset=['pts_diff']).groupby('pos')['pts_diff'].median()
+median_improvement_by_position = f_player_totals_df.dropna(subset=['pts_diff']).groupby('pos')['pts_diff'].median()
 
 min_games = 20
-player_totals_df_filtered = player_totals_df[player_totals_df['g'] >= min_games]
+player_totals_df_filtered = f_player_totals_df[player_totals_df['g'] >= min_games]
 median_improvement_filtered = player_totals_df_filtered.dropna(subset=['pts_diff']).groupby('pos')['pts_diff'].median()
 
 print("\nMedian improvement in points scored by position:")
